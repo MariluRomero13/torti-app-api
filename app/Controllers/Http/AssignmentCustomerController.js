671da76f54f46c567a5a17ce1cb332724630a6a8
mariluromero13 <romeromarilu13@gmail.com>
2020-03-26 21:26:09 -0600

    [UPD] products module

---
+++ app/Controllers/Http/AssignmentCustomerController.js
@@ -0,0 +1,93 @@
+'use strict'
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with assignmentcustomers
+ */
+class AssignmentCustomerController {
+  /**
+   * Show a list of all assignmentcustomers.
+   * GET assignmentcustomers
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index ({ request, response, view }) {
+  }
+
+  /**
+   * Render a form to be used for creating a new assignmentcustomer.
+   * GET assignmentcustomers/create
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async create ({ request, response, view }) {
+  }
+
+  /**
+   * Create/save a new assignmentcustomer.
+   * POST assignmentcustomers
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, response }) {
+  }
+
+  /**
+   * Display a single assignmentcustomer.
+   * GET assignmentcustomers/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show ({ params, request, response, view }) {
+  }
+
+  /**
+   * Render a form to update an existing assignmentcustomer.
+   * GET assignmentcustomers/:id/edit
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async edit ({ params, request, response, view }) {
+  }
+
+  /**
+   * Update assignmentcustomer details.
+   * PUT or PATCH assignmentcustomers/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ params, request, response }) {
+  }
+
+  /**
+   * Delete a assignmentcustomer with id.
+   * DELETE assignmentcustomers/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy ({ params, request, response }) {
+  }
+}
+
+module.exports = AssignmentCustomerController
--- app/Controllers/Http/ProductController.js
+++ app/Controllers/Http/ProductController.js
@@ -9,25 +9,43 @@
  */
 const Product = use('App/Models/Product')
 class ProductController {
-  async index ({ view }) {
-    const products = await Product.all()
-    return view.render('products.index', { products: products.toJSON() })
+  async index ({ view, params, request, response }) {
+    const page = params.page || 1
+    const search = request.input('search') || ''
+    const products = await Product.query()
+                                    .where('name', 'LIKE', '%' + search + '%')
+                                    .paginate(page, 5)
+    const pagination = products.toJSON()
+    pagination.route = 'products.pagination'
+    if(pagination.lastPage < page && page != 1) {
+      response.route(pagination.route, { page: 1 }, null, true)
+    }
+    else {
+      pagination.offset = (pagination.page - 1) * pagination.perPage
+      pagination.search = search
+      return view.render('products.index', { products: pagination })
+    }
   }
 
   async create ({ view }) {
     return view.render('products.create')
   }
 
-  async store ({ request, response }) {
+  async store ({ request, response, session }) {
     const product = request.only(Product.store)
-    await Product.create(product)
-    session.flash({
-      notification: {
-        type: 'success',
-        message: `Producto agregado correctamente`
-      }
-    })
-    return response.redirect('back')
+    try {
+      await Product.create(product)
+      return response.route('products.index')
+    } catch (error) {
+      session.flashOnly(['name', 'unit_price'])
+      session.flash({
+        notification: {
+          type: 'danger',
+          message: `Ocurrió un error, intentelo de nuevo`
+        }
+      })
+      return response.redirect('back')
+    }
   }
 
   async show ({ params, request, response, view }) {
@@ -40,6 +58,11 @@ class ProductController {
   }
 
   async destroy ({ params, request, response }) {
+  }
+
+  async getProducts({ response }) {
+    const products = await Product.all()
+    return response.ok(products)
   }
 }
 
