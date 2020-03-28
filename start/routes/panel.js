64cf83dfc7d3a33567afad499fd8c465fbb70e3f
mariluromero13 <romeromarilu13@gmail.com>
2020-03-27 21:29:33 -0600

    [ADD] show data dashboard and new items to navigation

--- start/routes/panel.js
+++ start/routes/panel.js
@@ -1,6 +1,7 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
+
 // Login
 Route.get('/', 'AuthController.showLoginForm').middleware(['authenticated'])
 Route.post('/login-panel', 'AuthController.loginPanel').as('login-panel').validator('Login')
@@ -9,14 +10,14 @@ Route.post('/logout-panel', 'AuthController.logoutPanel').as('logout-panel')
 // Home
 Route.get('home', 'HomeController.index').as('home').middleware('auth:session')
 
-//Products
+// Products
 Route.post('products', 'ProductController.store').as('products.store')
 .validator('Product/StoreUpdateProduct').middleware('auth:session')
 Route.get('products/create', 'ProductController.create').as('products.create').middleware('auth:session')
 Route.put('products/:id', 'ProductController.update').as('products.update')
 .validator('Product/StoreUpdateProduct').middleware('auth:session')
 Route.get('products/:id/edit', 'ProductController.edit').as('products.edit').middleware('auth:session')
-Route.get('products-page/:page?', 'ProductController.index').as('products.pagination')
+// Route.get('products-page/:page?', 'ProductController.index').as('products.pagination')
 
 //Roles
 Route.get('roles', 'RoleController.index').as('roles.index').middleware('auth:session')
@@ -25,4 +26,5 @@ Route.get('roles/create','RoleController.create').as('roles.create').middleware(
 Route.post('roles', 'RolesController.store').as('roles.store')
 .validator('Role/StoreUpdateRole').middleware('auth:session')
 
-
+// Employees
+Route.get('employees', 'EmployeeController.index').as('employees.index').middleware('auth:session')
