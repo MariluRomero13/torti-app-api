f66fcda99f651ff8cc651ca92faa4862122bb147
MariluRomero13 <romeromarilu13@gmail.com>
2020-04-09 17:21:27 -0500

    [UPD] role module design

--- app/Controllers/Http/RoleController.js
+++ app/Controllers/Http/RoleController.js
@@ -10,9 +10,22 @@
 const Role = use('App/Models/Role')
 class RoleController {
 
-  async index ({ request, response, view }) {
-    const roles = await Role.all()
-    return view.render('roles.index', { roles: roles.toJSON() })
+  async index ({ view, params, request, response }) {
+    const page = params.page || 1
+    const search = request.input('search') || ''
+    const roles = await Role.query()
+                                    .where('name', 'LIKE', '%' + search + '%')
+                                    .paginate(page, 5)
+    const pagination = roles.toJSON()
+    pagination.route = 'roles.pagination'
+    if(pagination.lastPage < page && page != 1) {
+      response.route(pagination.route, { page: 1 }, null, true)
+    }
+    else {
+      pagination.offset = (pagination.page - 1) * pagination.perPage
+      pagination.search = search
+      return view.render('roles.index', { roles: pagination })
+    }
   }
 
 
@@ -25,7 +38,7 @@ class RoleController {
     const rol = request.only(Role.store)
     try {
       await Role.create(rol)
-      return response.route('roles.index')
+      return response.route('roles.pagination')
     } catch (error) {
       session.flashOnly(['name'])
       session.flash({
@@ -42,21 +55,15 @@ class RoleController {
   }
 
   async edit ({ params, request, response, view }) {
-    const rol = await Role.find(params.id)
-    return view.render('roles.edit',{
-      rol
-    })
-    
+    const role = await Role.find(params.id)
+    return view.render('roles.edit', { role: role.toJSON() })
   }
 
-
   async update ({ params, request, response }) {
-    const rol = await Role.find(params.id)
-    rol.name = request.input('name')
-
-    await rol.save()
-
-    return response.redirect('/roles')
+    const role = await Role.find(params.id)
+    role.name = request.input('name')
+    await role.save()
+    return response.route('roles.pagination')
   }
 
   async destroy ({ params, request, response }) {
--- start/routes/panel.js
+++ start/routes/panel.js
@@ -8,7 +8,6 @@ Route.post('/logout-panel', 'AuthController.logoutPanel').as('logout-panel')
 // Home
 Route.get('home', 'HomeController.index').as('home').middleware('auth:session')
 
-
 //Customers
 Route.get('customers','CustomerController.index').as('customers.index').middleware('auth:session')
 Route.get('customers/create','CustomerController.create').as('customers.create').middleware('auth:session')
@@ -17,9 +16,6 @@ Route.get('/customers/edit/:id','CustomerController.edit').as('customers.edit')
 Route.put('customers/:id','CustomerController.update').as('customers.update').validator('Customer/StoreUpdateCustomer')
 Route.delete('/customers/delete/:id?','CustomerController.destroy').as('customers.delete')
 Route.put('customers/location/:id','LocationController.updateOrCreate').as('locations.updateOrCreate').validator('Location/StoreUpdateLocation')
-//Route.post('/customers/update/:rol_id?','CustomerController.update').as('customers.update')
-
-
 
 // Employees
 Route.get('employees-page/:page?', 'EmployeeController.index').as('employees.pagination')
@@ -41,14 +37,13 @@ Route.get('products/:id/edit', 'ProductController.edit').as('products.edit').mid
 Route.get('products-page/:page?', 'ProductController.index').as('products.pagination')
 
 //Roles
-Route.get('roles', 'RoleController.index').as('roles.index').middleware('auth:session')
+Route.get('roles-page/:page?', 'RoleController.index').as('roles.pagination')
 Route.get('roles/create','RoleController.create').as('roles.create').middleware('auth:session')
 Route.post('roles', 'RoleController.store').as('roles.store')
 .validator('Role/StoreUpdateRole').middleware('auth:session')
-Route.get('/roles/edit/:id','RoleController.edit').as('roles.edit')
-Route.put('roles/update/:id','RoleController.update').as('roles.update').validator('Role/StoreUpdateRole')
+Route.get('roles/:id/edit', 'RoleController.edit').as('roles.edit').middleware('auth:session')
+Route.put('roles/:id', 'RoleController.update').as('roles.update').validator('Role/StoreUpdateRole')
 Route.delete('/roles/delete/:id?','RoleController.destroy').as('roles.delete')
-//Route.post('/roles/update/:rol_id?','RoleController.update').as('roles.update')
 
 // Sales
 Route.get('sales-page/:page?', 'SaleController.index').as('sales.pagination')
