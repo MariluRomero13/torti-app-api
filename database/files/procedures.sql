9576cdc2c77146612b1ef3eff89721e365691cfc
cynthia_macias <cinthia.gomezmacias@gmail.com>
2020-04-20 21:25:30 -0500

    [UPDATE] procedures file

--- database/files/procedures.sql
+++ database/files/procedures.sql
@@ -1,5 +1,23 @@
 # Este procedimiento me trae todas las rutas de un repartidor en un día en especifico con o sin pagos pendientes
+
 DELIMITER $$
+CREATE procedure get_routes_employee
+(
+	current_day int,
+	employee int
+)
+begin
+	select  c.id, c.name as customer, IF(t1.pendiente = 1, t1.pendiente, 0) as pending_payment, t1.payment_id from assignment_customers as ac
+    inner join employees as e on e.id = ac.employee_id
+	inner join customers as c on c.id = ac.customer_id left join (select c.id as customer_id, count(pp.id) as pendiente, pp.id as payment_id
+	from customers as c inner join pending_payments as pp on pp.customer_id = c.id where pp.status = 1) as t1 on t1.customer_id = c.id
+	where e.id = employee and ac.day = current_day and ac.status = 1;
+end $$
+DELIMITER $$
+
+call get_routes_employee (1,1);
+
+/*DELIMITER $$
 CREATE procedure get_routes_employee
 (
 	current_day int,
@@ -14,7 +32,7 @@ begin
 end $$
 DELIMITER $$
 
-call get_routes_employee (1,1);
+call get_routes_employee (1,1);*/
 
 #Procedimiento que obtiene los clientes a los que ya se les realizó la venta por empleado y día
 DELIMITER $$
